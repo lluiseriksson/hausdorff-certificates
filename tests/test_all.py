0ef6a6ff1500c1f9f0410a4e12fa4f977950c308
lluiseriksson <lluiseriksson@gmail.com>
2026-07-05 20:40:44 +0200

    Handle short verifier witness vectors (#15)

--- tests/test_all.py
+++ tests/test_all.py
@@ -237,6 +237,17 @@ def test_certificate_malformed_witness_reports_failure():
     assert "'value'" in msg
 
 
+def test_certificate_short_witness_reports_dimension_failure():
+    b = moments_from_atoms([(F(1, 2), F(1, 3)), (F(1, 2), F(2, 3))], 12)
+    cert = certify_exact("bad_theta", b, "hankel_L_theta", 5, theta=F(1, 2))
+    obj = json.loads(cert.to_json())
+    obj["certificate"]["v"] = obj["certificate"]["v"][:-1]
+
+    ok, msg = verify_obj(obj)
+    assert not ok
+    assert msg == "witness vector length 5 does not match matrix size 6"
+
+
 def test_certificate_roundtrip_interval():
     import mpmath
 
